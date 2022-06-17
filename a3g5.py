246f3d5e728287ec3830f777e27cae044fd350f3
Romina D. Romano <roderomano@gmail.com>
2022-06-17 11:30:12 -0300

    Creación de palicación a3g5.py

--- a3g5.py
+++ a3g5.py
@@ -1,1 +1,44 @@
-print("hola")
+# 1) aplicación a3g5.py
+print("Bienvenidos/as a la App a3g5.py")
+
+print("\nLlamado a la función presentacion...")
+# 2) colocar llamado a presentacion()
+
+print("\nLlamado a la función ing2i...")
+# 3) colocar llamado a ing2i()
+
+print("\nLlamado a la función ing2s...")
+# 4) colocar llamado a ing2s()
+
+print("\nLlamado a la función suma...")
+# 5) colocar llamado a suma(a,b)
+
+print("\nLlamado a la función resta...")
+# 6) colocar llamado a resta(a,b)
+
+print("\nLlamado a la función producto...")
+# 7) colocar llamado a producto(a,b)
+
+print("\nLlamado a la función cociente...")
+# 8) colocar llamado a cociente(a,b)
+
+print("\nLlamado a la función modulo...")
+# 9) colocar llamado a modulo(a,b)
+
+print("\nLlamado a la función potencia...")
+# 10) colocar llamado a potencia(a,b)
+
+print("\nLlamado a la función radicacion...")
+# 11) colocar llamado a radicacion(a,b)
+
+print("\nLlamado a la función operacion1: producto de los 2 primeros más el 3er ...")
+# 12) colocar llamado a operacion1(a,b,c)
+
+print("\nLlamado a la función operacion2: suma de los 2 primeros por el 3er ...")
+# 13) colocar llamado a operacion2(a,b,c)
+
+print("\nLlamado a la función operacion3: resta de los 2 primeros por el 3er ...")
+# 14) colocar llamado a operacion3(a,b,c)
+
+print("\nLlamado a la función operacion4: producto de los 2 primeros menos el 3er  ...")
+# 15) colocar llamado a operacion4(a,b,c)
